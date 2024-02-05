369825979929ebddd515737e5a6affbe1d2eb3d4
tejakodamala9 <tejakodamala@gmail.com>
2024-02-05 11:30:58 -0500

    added sysout

--- src/main/java/com/payment/PaymentServiceApplication.java
+++ src/main/java/com/payment/PaymentServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("paymentservice");
+		System.out.println("payment service");
 		SpringApplication.run(PaymentServiceApplication.class, args);
 	}
 
